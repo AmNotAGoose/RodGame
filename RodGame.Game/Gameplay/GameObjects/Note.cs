9497b219ef0e0b0fe5c301fe4aad6656d811e13e
AmNotAGoose <57881736+AmNotAGoose@users.noreply.github.com>
2025-12-01 21:15:32 -0500

    add hitsounds

--- RodGame.Game/Gameplay/GameObjects/Note.cs
+++ RodGame.Game/Gameplay/GameObjects/Note.cs
@@ -1,5 +1,6 @@
 using System;
 using osu.Framework.Allocation;
+using osu.Framework.Audio.Sample;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Shapes;
@@ -9,9 +10,9 @@ using osu.Framework.Input;
 using osu.Framework.Screens;
 using osuTK;
 using osuTK.Graphics;
-using osuTK.Graphics.ES10;
 using RodGame.Game.Gameplay.Models;
 using SharpGen.Runtime.Trimming;
+using Veldrid;
 
 namespace RodGame.Game.Gameplay.GameObjects
 {
@@ -19,17 +20,22 @@ namespace RodGame.Game.Gameplay.GameObjects
     {
         public NoteModel Model;
 
+        private Sample hitSound;
+
         public const double FADE_IN_TIME = 1000;
         public const double FADE_OUT_TIME = 200;
+        public const double HIT_WINDOW = 50;
 
         [Resolved] private ChartSong gameSong { get; set; }
 
         private Rod parentRod;
+        private bool hasBeenHit = false;
 
 
         [BackgroundDependencyLoader]
-        private void load(IRenderer renderer)
+        private void load(IRenderer renderer, ISampleStore samples)
         {
+            hitSound = samples.Get("hitsound.wav");
             Texture = renderer.WhitePixel;
             RelativeSizeAxes = Axes.None;
             RelativePositionAxes = Axes.None;
@@ -51,7 +57,19 @@ namespace RodGame.Game.Gameplay.GameObjects
             base.Update();
 
             double currentTime = gameSong.Song.CurrentTime;
-            double timeDifference = currentTime - Model.StartTime; 
+            double timeDifference = currentTime - Model.StartTime;
+
+
+            if (!hasBeenHit && Math.Abs(timeDifference) <= HIT_WINDOW)
+            {
+                hasBeenHit = true;
+                hitSound?.Play();
+            }
+
+            if (hasBeenHit && timeDifference < -HIT_WINDOW)
+            {
+                hasBeenHit = false;
+            }
 
             if (timeDifference < -FADE_IN_TIME)
             {
